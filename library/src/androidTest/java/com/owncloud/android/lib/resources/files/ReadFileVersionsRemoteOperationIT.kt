d04786b1be70f22857fc860ec423d98ad1ad04e8
tobiasKaminsky <tobias@kaminsky.me>
2023-03-10 14:08:44 +0100

    Update file version
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- library/src/androidTest/java/com/owncloud/android/lib/resources/files/ReadFileVersionsRemoteOperationIT.kt
+++ library/src/androidTest/java/com/owncloud/android/lib/resources/files/ReadFileVersionsRemoteOperationIT.kt
@@ -36,12 +36,13 @@ class ReadFileVersionsRemoteOperationIT : AbstractIT() {
     fun listVersions() {
         val txtFile = getFile(ASSETS__TEXT_FILE_NAME)
         val filePath = "/test.md"
+        val mimetype = "txt/plain"
 
         var uploadResult =
             UploadFileRemoteOperation(
                 txtFile.absolutePath,
                 filePath,
-                "txt/plain",
+                mimetype,
                 System.currentTimeMillis() / MILLI_TO_SECOND
             )
                 .execute(client)
@@ -62,7 +63,7 @@ class ReadFileVersionsRemoteOperationIT : AbstractIT() {
             // with NC26+ we always have a starting version
             versionCount++
         }
-        assertEquals(versionCount, sutResult.data.size)
+        assertEquals(versionCount, sutResult.resultData.size)
 
         // modify file to have a version
         FileWriter(txtFile).apply {
@@ -75,7 +76,7 @@ class ReadFileVersionsRemoteOperationIT : AbstractIT() {
             UploadFileRemoteOperation(
                 txtFile.absolutePath,
                 filePath,
-                "txt/plain",
+                mimetype,
                 System.currentTimeMillis() / MILLI_TO_SECOND
             )
                 .execute(client)
@@ -89,6 +90,11 @@ class ReadFileVersionsRemoteOperationIT : AbstractIT() {
         assertTrue(sutResult.isSuccess)
 
         versionCount++
-        assertEquals(versionCount, sutResult.data.size)
+        val versions = sutResult.resultData
+        assertEquals(versionCount, versions.size)
+        versions[0].apply {
+            assertTrue(fileLength > 0)
+            assertEquals(mimetype, mimeType)
+        }
     }
 }
